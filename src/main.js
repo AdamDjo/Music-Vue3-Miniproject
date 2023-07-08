dbf550ea2f4c90182506e1b55168da96a4464eda
Adam Djo <adambenmessaoud2@gmail.com>
2023-07-08 17:21:24 +0100

    add rules of forms

--- src/main.js
+++ src/main.js
@@ -4,10 +4,12 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 //import router from './router'
+import Veevalidate from './plugins/vee-validate/validation'
 
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(Veevalidate)
 //app.use(router)
 
 app.mount('#app')

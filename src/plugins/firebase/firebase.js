7bd978562045b28cbdf3fd2ca455452b4a56e902
Adam Djo <adambenmessaoud2@gmail.com>
2023-10-01 16:26:09 +0100

    adding function of Uploads & progress bar of upload

--- src/plugins/firebase/firebase.js
+++ src/plugins/firebase/firebase.js
@@ -1,27 +1,24 @@
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/firestore';
-
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+import 'firebase/storage'
 const firebaseConfig = {
-  apiKey: "AIzaSyCZydwwglV1Mpw42h9yVOOAI-ozEZUniGU",
-  authDomain: "music-32553.firebaseapp.com",
-  projectId: "music-32553",
-  storageBucket: "music-32553.appspot.com",
-  appId: "1:593162951225:web:e4b863cbb913a9bc300be8",
-};
+  apiKey: 'AIzaSyCZydwwglV1Mpw42h9yVOOAI-ozEZUniGU',
+  authDomain: 'music-32553.firebaseapp.com',
+  projectId: 'music-32553',
+  storageBucket: 'music-32553.appspot.com',
+  appId: '1:593162951225:web:e4b863cbb913a9bc300be8'
+}
 
 // Initialize Firebase
 if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
+  firebase.initializeApp(firebaseConfig)
 }
 
-const auth = firebase.auth();
-const db = firebase.firestore();
+const auth = firebase.auth()
+const db = firebase.firestore()
+const storage = firebase.storage()
 
-const usersCollection = db.collection('users');
+const usersCollection = db.collection('users')
 
-export {
-  auth,
-  db,
-  usersCollection
-};
+export { auth, db, usersCollection, storage }
